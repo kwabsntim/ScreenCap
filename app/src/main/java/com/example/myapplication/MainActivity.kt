7447034183a566992525f8c5832c3fd576ad6197
kwabsntim <daffourkwabena@gmail.com>
2026-02-28 18:13:55 +0000

    add permission requests for audio, storage, and notifications; integrate system UI controller and update button logic

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -22,6 +22,14 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.Alignment
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import androidx.compose.runtime.SideEffect
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
 
 class MainActivity : ComponentActivity() {
@@ -29,35 +37,75 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val systemUiController = rememberSystemUiController()
+            val audioPermission = android.Manifest.permission.RECORD_AUDIO
+            val storagePermission = android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+
+            var hasAudioPermission by remember { mutableStateOf(false) }
+            var hasStoragePermission by remember { mutableStateOf(false) }
+
+            val audioPermissionLauncher =
+                rememberLauncherForActivityResult(contract = ActivityResultContracts.RequestPermission()) { isGranted ->
+                    hasAudioPermission = isGranted
+                }
+
+            val storagePermissionLauncher = rememberLauncherForActivityResult(
+                contract = ActivityResultContracts.RequestPermission()
+            ) { isGranted ->
+                hasStoragePermission = isGranted
+            }
+            SideEffect {
+                systemUiController.setStatusBarColor(
+                    color = Color.White,
+                    darkIcons = true // dark icons for light background
+                )
+            }
             MyApplicationTheme {
-                Box(modifier=Modifier.fillMaxSize().background(Color.White), contentAlignment = Alignment.Center){
-                    Scaffold(modifier = Modifier.fillMaxSize(), containerColor = Color.Transparent) { innerPadding ->
+                Box(
+                    modifier = Modifier.fillMaxSize().background(Color.White),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Scaffold(
+                        modifier = Modifier.fillMaxSize(),
+                        containerColor = Color.Transparent
+                    ) { innerPadding ->
+                        //wrapping the button in a box to center it on the screen
                         Box(
-                            modifier=Modifier.fillMaxSize(),
-                            contentAlignment=Alignment.Center
+                            modifier = Modifier.fillMaxSize(),
+                            contentAlignment = Alignment.Center
                         ) {
                             FilledButtonExample(
                                 modifier = Modifier.padding(innerPadding),
-                                onClick = {})
-                        }
+                                onClick = {
+                                    if (!hasAudioPermission) {
+                                        audioPermissionLauncher.launch(audioPermission)
+                                    } else if (!hasStoragePermission) {
+                                        storagePermissionLauncher.launch(storagePermission)
+                                    }
+                                })
 
+                        }
                     }
-                }
 
 
                 }
             }
         }
     }
-@Composable
-fun FilledButtonExample(onClick: () -> Unit,modifier: Modifier=Modifier) {
-    //wrapping the button in a box to center it on the screen
-    Button(
-        onClick = { onClick() },
-        modifier = modifier.size(90.dp),
-        shape= CircleShape,
-        colors=ButtonDefaults.buttonColors(containerColor = Color.Red,contentColor=Color.White),
-    ) {
-        Text("Record")
+
+    @Composable
+    fun FilledButtonExample(onClick: () -> Unit, modifier: Modifier = Modifier) {
+        //wrapping the button in a box to center it on the screen
+        Button(
+            onClick = { onClick() },
+            modifier = modifier.size(90.dp),
+            shape = CircleShape,
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Red,
+                contentColor = Color.White
+            ),
+        ) {
+            Text("Record")
+        }
     }
 }
